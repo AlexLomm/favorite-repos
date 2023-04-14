568e0dc8ca1bde854fea9a1951d537a12705f1db
Alex Lomia <alex.lomia@gmail.com>
2023-04-13 23:58:01 -0400

    Center <GithubRepositoriesSearch /> inside Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { css } from '@emotion/react';
+
 import GithubRepositoriesSearch from '../../components/GithubRepositoriesSearch';
 
-const Home = () => <GithubRepositoriesSearch />;
+const Home = () => {
+  return (
+    <div
+      css={css`
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      <GithubRepositoriesSearch />
+    </div>
+  );
+};
 
 export default Home;
