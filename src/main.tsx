1b97564faac11eb95e4fb10b33a5ea4379bf4a2d
Alex Lomia <alex.lomia@gmail.com>
2023-04-13 22:43:39 -0400

    Add comments to css imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// reset the default browser styles
 import 'normalize.css/normalize.css';
+
+// import Roboto font, as it's used by Material-UI.
+// @see https://mui.com/material-ui/getting-started/installation/#roboto-font
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+// import global styles
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
