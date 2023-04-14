27710f39860a4d9bab8a192e94cb3fdbee71a1dd
Alex Lomia <alex.lomia@gmail.com>
2023-04-14 01:16:03 -0400

    Add comments to svgr.d.ts

--- src/svgr.d.ts
+++ src/svgr.d.ts
@@ -1,3 +1,7 @@
+// these types are required in order for Typescript not to complain when importing
+// svg icons like this:
+//
+// `import { ReactComponent as WhateverIcon } from './icons/WhateverIcon.svg';`
 declare module "*.svg" {
   import * as React from "react";
 
