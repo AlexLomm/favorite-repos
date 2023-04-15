6dbfc47d31a13b935bb7e31b5a8f2d421a89581d
Alex Lomia <alex.lomia@gmail.com>
2023-04-15 00:24:45 -0400

    Fix validate-schema.ts

--- src/utils/validate-schema.ts
+++ src/utils/validate-schema.ts
@@ -2,15 +2,9 @@ import { ZodSchema } from 'zod';
 
 /**
  * A function that validates a schema and returns the data if it matches the schema.
- *
- * If the environment is not development, the data is returned as-is for performance reasons.
  */
 const validateSchema = <T>(schema: ZodSchema, data: unknown): T => {
-  if (process.env.NODE_ENV === 'development') {
-    return schema.parse(data);
-  }
-
-  return data as T;
+  return schema.parse(data);
 };
 
 export default validateSchema;
